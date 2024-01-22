5d24846a6cbedf1b4ad35ec5c78b98164075958a
Tanner <tvandewalle@microsoft.com>
2024-01-21 20:04:00 -0800

    Initial source

---
+++ ExtendedShortcuts/Commands/MyCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtendedShortcuts
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("ExtendedShortcuts", "Button clicked");
+        }
+    }
+}
